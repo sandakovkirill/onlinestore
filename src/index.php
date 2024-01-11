7bb8deb04fefdd0a402ffc620f26aeec6e212723
Kirill Sandakov <ksandakov2004@gmail.com>
2024-01-11 10:45:11 +0700

    first_page

---
+++ src/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Store</title>
+    <link rel="stylesheet" href="style/css/normalize.css">
+    <link rel="stylesheet" href="style/css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <div class="header__inner">
+            <div class="header__top">
+                <div class="container">
+                    <div class="header__top-inner">
+                        <div class="logo">
+                            <img src="icons/Logo.svg" alt="logo-company">
+                        </div>
+                        <nav>
+                            <ul>
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">Shop</a></li>
+                                <li><a href="#">About</a></li>
+                                <li><a href="#">Blog</a></li>
+                                <li><a href="#">Contact</a></li>
+                            </ul>
+                        </nav>
+                        <div class="navbar">
+                            <ul>
+                                <li class="navbar__element"> <img src="icons/user.svg" alt="user-icon"></li>
+                                <li class="navbar__element"> <img src="icons/Cart.svg" alt="shop-basket"></li>
+                                <li class="navbar__element"> <img src="icons/Cart.svg" alt="shop-basket"></li>
+                                <li class="navbar__element"> <img src="icons/Cart.svg" alt="shop-basket"></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="header__bottom">
+                <div class="container">
+                    <div class="header__bottom-inner">
+                        <div class="advertising-offer">
+                            <h5>New Arrival</h5>
+                            <h1>Discover Our New Collection</h1>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
+                            <button>BUY NOW</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
